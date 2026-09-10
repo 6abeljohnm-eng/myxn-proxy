aeb277957c4d68e36cd29817bafff4c512aa2fa5
6abeljohnm-eng <6abel.johnm@gmail.com>
2026-09-11 07:08:39 +1000

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,8 +1,16 @@
 { pkgs }: {
   deps = [
-    pkgs.nodejs_18
-    pkgs.npm
+    pkgs.nodejs-18_x
+    pkgs.nodePackages.npm
+    pkgs.git
+    pkgs.curl
+    pkgs.wget
+    pkgs.openssl
   ];
-  env = {};
-  run = "npm install && npm start";
+
+  env = {
+    NODEJS_VERSION = "18";
+    npm_config_update_notifier = "false";
+    PATH = "/opt/nodejs/18/bin:$PATH";
+  };
 }
